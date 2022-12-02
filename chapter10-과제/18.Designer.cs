8cd7612dc6d799f01dc3d17ec7b0fe1d3c28bbb2
hyojin park <candy020406@naver.com>
2022-12-02 18:14:12 +0900

    커밋

---
+++ chapter10-과제/18.Designer.cs
@@ -0,0 +1,49 @@
+﻿namespace chapter10_과제
+{
+    partial class _18
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.SuspendLayout();
+            // 
+            // _18
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(7F, 12F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(384, 361);
+            this.Name = "_18";
+            this.Text = "Paint with the Mouse";
+            this.FormClosed += new System.Windows.Forms.FormClosedEventHandler(this._18_FormClosed);
+            this.Load += new System.EventHandler(this._18_Load);
+            this.MouseMove += new System.Windows.Forms.MouseEventHandler(this._18_MouseMove);
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+    }
+}
---
+++ chapter10-과제/18.cs
@@ -0,0 +1,42 @@
+﻿using System; //10장.보충내용 마우스 이벤트 사용예제
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace chapter10_과제
+{
+    public partial class _18 : Form
+    {
+        public _18()
+        {
+            InitializeComponent();
+        }
+
+        private Graphics m_objGraphics; //Graphics 객체 참조 변수 선언
+        private void _18_Load(object sender, EventArgs e)
+        {
+            m_objGraphics = this.CreateGraphics();
+        }
+
+        private void _18_FormClosed(object sender, FormClosedEventArgs e)
+        {
+            m_objGraphics.Dispose(); //그래픽 객체 자원 반납
+        }
+
+        private void _18_MouseMove(object sender, MouseEventArgs e) //그림 그리기 구현 메소드
+        {
+            Rectangle rectEllipse = new Rectangle();
+            if (e.Button != MouseButtons.Left) return; // 왼쪽 마우스 버튼이 아닐 경우 바로 종료
+            rectEllipse.X = e.X - 1;
+            rectEllipse.Y = e.Y - 1;
+            rectEllipse.Width = 2;
+            rectEllipse.Height = 2;
+            m_objGraphics.DrawEllipse(System.Drawing.Pens.Blue, rectEllipse);
+        }
+    }
+}
