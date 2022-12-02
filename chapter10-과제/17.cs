bbc837875a1e9125b5e44ea02355df13061efde7
hyojin park <candy020406@naver.com>
2022-12-02 18:08:07 +0900

    커밋

--- chapter10-과제/17.cs
+++ chapter10-과제/17.cs
@@ -19,8 +19,9 @@ namespace chapter10_과제
 
         private void textBox1_KeyPress(object sender, KeyPressEventArgs e)
         {
+            //눌려진 키는 e매개변수의 KeyChar속성을 통해 알 수 있음
             if ((e.KeyChar == 'k') || (e.KeyChar == 'K')) // k를 누르는 경우(대문자,소문자 상관 x)
-                e.Handled = true; //키가 무시됨(별도처리 설정)
+                e.Handled = true; //별도처리 설정 -> 키가 무시됨
         }
     }
 }
