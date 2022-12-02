5ecb352d0fda35c0c3a05180b0e3c1c41d59c88f
hyojin park <candy020406@naver.com>
2022-12-02 18:06:13 +0900

    커밋

--- chapter10-과제/17.cs
+++ chapter10-과제/17.cs
@@ -1,4 +1,4 @@
-﻿using System;
+﻿using System; //10장. 보충내용 문자 k를 받아들이지 않는 텍스트 박스 구현
 using System.Collections.Generic;
 using System.ComponentModel;
 using System.Data;
@@ -20,7 +20,7 @@ namespace chapter10_과제
         private void textBox1_KeyPress(object sender, KeyPressEventArgs e)
         {
             if ((e.KeyChar == 'k') || (e.KeyChar == 'K')) // k를 누르는 경우(대문자,소문자 상관 x)
-                e.Handled = true;
+                e.Handled = true; //키가 무시됨(별도처리 설정)
         }
     }
 }
