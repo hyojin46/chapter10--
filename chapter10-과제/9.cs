a3c73533365f944a510cb00f8d260162b8190b47
hyojin park <candy020406@naver.com>
2022-12-02 17:44:41 +0900

    커밋

--- chapter10-과제/9.cs
+++ chapter10-과제/9.cs
@@ -22,7 +22,7 @@ namespace chapter10_과제
             MessageBox.Show("비밀번호를 입력하세요"); //메시지박스 띄우기
         }
 
-        private void textBox2_Enter(object sender, EventArgs e) //두번째 텍스트 박스를 누르는 경우
+        private void textBox2_Enter(object sender, EventArgs e) //두번째 텍스트 박스를 누르는 경우 -> 두번째 텍스트박스가 입력포커스를 가짐
         {
             MessageBox.Show("두번째 TextBox 입력");
         }
