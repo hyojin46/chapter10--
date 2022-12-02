8497e8deeefb2ce01cf4c2cb5baa53f377013c0d
hyojin park <candy020406@naver.com>
2022-12-02 17:42:39 +0900

    커밋

--- chapter10-과제/5.cs
+++ chapter10-과제/5.cs
@@ -14,7 +14,7 @@ namespace chapter10_과제
     {
         public _5() //생성자
         {
-            InitializeComponent();
+            InitializeComponent(); //초기화
         }
 
         private void _5_MouseEnter(object sender, EventArgs e)
